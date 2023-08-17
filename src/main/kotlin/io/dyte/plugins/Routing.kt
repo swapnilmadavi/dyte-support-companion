b74ba33db251ebb728b349bf4158a530f3ab319c
Swapnil Madavi <swapnil@dyte.io>
2023-08-17 18:56:51 +0530

    chore: remove used imports

--- src/main/kotlin/io/dyte/plugins/Routing.kt
+++ src/main/kotlin/io/dyte/plugins/Routing.kt
@@ -2,7 +2,6 @@ package io.dyte.plugins
 
 import io.dyte.routes.codeSubmissionRoute
 import io.ktor.server.application.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
